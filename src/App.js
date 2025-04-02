bcf146af1dcbf8f60722ee133f81820f8fd188be
RichaCui70 <richacui70@gmail.com>
2025-04-01 22:19:40 -0400

    NEW: Graph and chart added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react"
 import {
   FaAngleLeft,
   FaAngleRight,
@@ -6,11 +7,34 @@ import {
   FaBell,
   FaPlus,
 } from "react-icons/fa6"
-// import { Line } from "react-chartjs-2"
+import { Line } from "react-chartjs-2"
+import { CategoryScale } from "chart.js"
+import Chart from "chart.js/auto"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
 
 import "./App.css"
 import Card from "./components/Card"
 
+Chart.register(CategoryScale)
+
+const dummyRows = [
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+  { device: "Fridge", output: 23 },
+]
+const dummyChartData = [372, 424, 636, 344, 553, 223]
 function App() {
   return (
     <div className="App">
@@ -71,20 +95,80 @@ function App() {
         </div>
       </div>
       <div className="second-row">
-        {/* <Line
-          data={{
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-            datasets: [
-              {
-                label: "My First Dataset",
-                data: [65, 59, 80, 81, 56, 55],
-                fill: false,
-                borderColor: "rgb(75, 192, 192)",
-                tension: 0.1,
+        <div className="chart-container">
+          <Line
+            data={{
+              labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+
+              datasets: [
+                {
+                  label: "Users Gained ",
+                  data: dummyChartData,
+                  backgroundColor: [
+                    "rgba(75,192,192,1)",
+                    "#ecf0f1",
+                    "#f0331a",
+                    "#f3ba2f",
+                    "#2a71d0",
+                    "#2a22d0",
+                  ],
+                  borderColor: "black",
+                  borderWidth: 2,
+                },
+              ],
+            }}
+            options={{
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Users Gained between 2016-2020",
+                },
+                legend: {
+                  display: false,
+                },
               },
-            ],
-          }}
-        /> */}
+            }}
+          />
+        </div>
+        <div style={{}}>
+          <TableContainer component={Paper}>
+            <Table
+              sx={{ minWidth: 650 }}
+              size="small"
+              aria-label="a dense table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    sx={{ borderBottom: "1px solid #000000" }}
+                    align="center"
+                  >
+                    Device
+                  </TableCell>
+                  <TableCell
+                    sx={{ borderBottom: "1px solid #000000" }}
+                    align="center"
+                  >
+                    Output&nbsp;(kwh)
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {dummyRows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" align="center" scope="row">
+                      {row.device}
+                    </TableCell>
+                    <TableCell align="center">{row.output}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
     </div>
   )
